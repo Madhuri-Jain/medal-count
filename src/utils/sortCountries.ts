68268956852f7724c9f12cc0d7a9818f9da49f28
Madhuri Jain <madhuri.jain77@gmail.com>
2025-07-04 15:36:59 +0100

    added one mor elayer of tie breaker in case the tie breaker become tie

--- src/utils/sortCountries.ts
+++ src/utils/sortCountries.ts
@@ -7,12 +7,16 @@ export function sortCountries(
     return [...data].sort((a, b) => {
         const bTotal = b.gold + b.silver + b.bronze;
         const aTotal = a.gold + a.silver + a.bronze;
-        if (sortBy === 'total') {
-            return bTotal - aTotal || b.gold - a.gold;
-        }
-        if (sortBy === 'gold') return b.gold - a.gold || b.silver - a.silver;
-        if (sortBy === 'silver') return b.silver - a.silver || b.gold - a.gold;
-        if (sortBy === 'bronze') return b.bronze - a.bronze || b.gold - a.gold;
+
+        if (sortBy === 'total')
+            return bTotal - aTotal || b.gold - a.gold || b.silver - a.silver;
+        if (sortBy === 'gold')
+            return b.gold - a.gold || b.silver - a.silver || bTotal - aTotal;
+        if (sortBy === 'silver')
+            return b.silver - a.silver || b.gold - a.gold || bTotal - aTotal;
+        if (sortBy === 'bronze')
+            return b.bronze - a.bronze || b.gold - a.gold || bTotal - aTotal;
+
         return 0;
     });
 }
