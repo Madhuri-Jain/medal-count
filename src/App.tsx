a5d0854fb32844edba6c00dba4b622a15521f8d7
Madhuri Jain <madhuri.jain77@gmail.com>
2025-07-04 13:31:03 +0100

    configured ESLint and Prettier tools for code formatting and consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <>
-      <h1 className="text-red-200 dark:text-amber-300">Medal Count</h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-red-200 dark:text-amber-300">Medal Count</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
