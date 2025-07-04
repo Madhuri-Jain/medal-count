c30e5af3c70f9f8adcbbc4a8521b7e8fa918a644
Madhuri Jain <madhuri.jain77@gmail.com>
2025-07-04 14:07:54 +0100

    api setup to fetch the medals data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useMedalData } from './hooks/useMedalData';
+
 function App() {
+    const { data, error, loading } = useMedalData();
     return (
         <>
             <h1 className="text-red-200 dark:text-amber-300">Medal Count</h1>
+            {/* testing data load */}
+            {loading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>{error}</p>
+            ) : (
+                <p>{JSON.stringify(data)}</p>
+            )}
         </>
     );
 }
